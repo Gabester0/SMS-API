934bf29f1cd15967044d38d1e5d5b714b0a9dc4c
gabester0 <gceipper@gmail.com>
2025-08-21 15:10:12 -0400

    skip auth for healthcheck

--- app/controllers/rails/health_controller.rb
+++ app/controllers/rails/health_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 
+
 module Rails
   class HealthController < ActionController::API
+    skip_before_action :authenticate_user!, raise: false
+
     def show
       head :ok
     end
