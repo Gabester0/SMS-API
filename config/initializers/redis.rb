c0db8864f902a1bffdbc735adc6e3fd680c0963f
gabester0 <gceipper@gmail.com>
2025-08-21 17:26:39 -0400

    updating redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,9 +3,8 @@ require 'redis'
 redis_config = {
   url: ENV.fetch('REDIS_URL'),
   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE },
-  reconnect_attempts: 3,
-  reconnect_delay: 0.5,
-  reconnect_delay_max: 2.0
+  retry_count: 3,
+  timeout: 1
 }
 
 begin
