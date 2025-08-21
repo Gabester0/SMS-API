9728948389220183b633173194c26cc64c40be2b
gabester0 <gceipper@gmail.com>
2025-08-21 14:37:36 -0400

    updating to use twilio env variables

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,5 @@
 Rails.application.config.x.twilio = {
-  account_sid: Rails.application.credentials.twilio&.account_sid,
-  auth_token: Rails.application.credentials.twilio&.auth_token,
-  phone_number: Rails.application.credentials.twilio&.phone_number || '+15005550006'
+  account_sid: ENV['TWILIO_ACCOUNT_SID'] || Rails.application.credentials.twilio&.account_sid,
+  auth_token: ENV['TWILIO_AUTH_TOKEN'] || Rails.application.credentials.twilio&.auth_token,
+  phone_number: ENV['TWILIO_PHONE_NUMBER'] || Rails.application.credentials.twilio&.phone_number || '+15005550006'
 }
